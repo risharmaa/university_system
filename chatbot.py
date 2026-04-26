fc9e0bf20e5ff53fc0a68e6463a41c55a692ede6
Riya Sharma <riya@Riyas-MacBook-Air.local>
2026-04-26 00:46:44 -0400

    changed the code in chatbot.py to add a function (advising_chat) for us to be able to call in main.py. function advising_chat also takes in a system instruction that we can use to make sure that the chatbot only answers questions about advising.

--- chatbot.py
+++ chatbot.py
@@ -12,8 +12,29 @@ except AttributeError:
     print("Error! The GEMINI_API_KEY was not found.")
     exit()
 
-# creating the generative model
-model = genai.GenerativeModel('gemini-2.5-flash')
+
+with open("schema.sql", "r") as f:
+    sql_schema = f.read()
+
+def advising_chat(user_input, uid):
+    config=types.GenerateContentConfig(
+        system_instruction=f"""You are a college advisor. Using this SQL schema: {sql_schema}, help students (defined by {uid}) 
+        to pick classes based on their interests using the courses table. Before submitting your suggestion, check the 
+        courses_offered table to make sure that the class is offered this semester. Also check the enrollment table for
+        the student using {uid} to see if the student has already taken that class (do not recommend that class!)
+        """
+    )
+
+    # creating the generative model
+    model = genai.GenerativeModel(model_name = 'gemini-2.5-flash', system_instruction = system_instruction)
+
+    try:
+        # Call the model
+        response = model.generate_content(user_input)
+        return response.text
+    except Exception as e:
+        return f"Sorry, I encountered an error: {str(e)}"
+
 
 # Start a chat session with no context/history
 chat = model.start_chat(history = [])
